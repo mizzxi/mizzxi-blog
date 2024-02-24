340bfb47b54a826a40775a2906515c3ee26a5925
미현 <83504363+mihyun-note@users.noreply.github.com>
2024-02-24 21:06:23 +0900

    header 오류 수정

--- til_blog_react/src/pages/post/tilExplain.js
+++ til_blog_react/src/pages/post/tilExplain.js
@@ -1,12 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import main_til from "../../assets/images/study/main_til.jpg";
 import "../../assets/css/post.css";
 import PostListSide from "../../components/postListSide";
 
 export default function TilExplain() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const bodyHeight = document.body.clientHeight;
+
+      const maxScroll = bodyHeight - windowHeight;
+      const percentage = (scrollPosition / maxScroll) * 100;
+
+      setProgress(percentage);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     /* jshint ignore:start */
     <>
+      <div
+        className="progress"
+        role="progressbar"
+        style={{
+          backgroundColor: "#ffff",
+        }}
+      >
+        <div
+          className="progress-bar barStyle"
+          style={{
+            width: progress + "%",
+            backgroundColor: "black",
+            position: "fixed",
+            height: 3 + "px",
+            zIndex: 2,
+            margin: 8 + "px",
+            borderRadius: 100 + "px",
+          }}
+        ></div>
+      </div>
       <div className="post">
         <div>
           <a>TIL</a>
